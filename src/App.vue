7f1fced0fc051c6f253d7885f9c201b8023d5ca1
PLOSKIREV <ploskirev@MacBook-Pro-PLOSKIREV.local>
2021-11-01 15:00:01 +0300

    Workshop#3 Hometask

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,134 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div @click="closePersonCard" id="app">
+    <div class="office">
+      <Map
+        :tables="tablesWithColorAndDepartment"
+        @tableSelected="setSelectedTable"
+      />
+      <SideMenu
+        @click.native.stop
+        @updateLegendSorting="updateLegendSorting"
+        @closePersonCard="closePersonCard"
+        :person="personWithDepartment"
+        :legend="legendWithLinkedCounter"
+      />
     </div>
+  </div>
 </template>
 
 <script>
-import Map from "./components/Map.vue";
-import SideMenu from "./components/SideMenu.vue";
+import people from '@/assets/data/people.json'
+import tables from '@/assets/data/tables.json'
+import legend from '@/assets/data/legend.json'
+import Map from './components/Map.vue'
+import SideMenu from './components/SideMenu.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     Map,
-    SideMenu,
+    SideMenu
   },
-};
+  data() {
+    return {
+      selectedTable: null,
+      people: [],
+      tables: [],
+      legend: []
+    }
+  },
+  created() {
+    this.loadLegend()
+    this.loadTables()
+    this.loadPeople()
+  },
+  computed: {
+    personWithDepartment() {
+      if (this.selectedTable === null) return null
+      const person =
+        this.people.find(person => person.tableId === this.selectedTable._id) ??
+        {}
+      return {
+        ...person,
+        department: this.selectedTable.departmentName
+      }
+    },
+    tablesWithColorAndDepartment() {
+      return this.tables.map(table => {
+        const department = this.legend.find(
+          el => el.group_id === table.group_id
+        )
+        const departmentName = department?.text ?? 'no name department'
+        const color = department?.color ?? 'transparent'
+        return { ...table, color, departmentName }
+      })
+    },
+    legendWithLinkedCounter() {
+      return this.legend.map(el => ({
+        ...el,
+        counter: this.tables.reduce(
+          (acc, cur) => (cur.group_id === el.group_id ? ++acc : acc),
+          0
+        )
+      }))
+    }
+  },
+  methods: {
+    loadPeople() {
+      this.people = people
+    },
+    loadTables() {
+      this.tables = tables
+    },
+    loadLegend() {
+      this.legend = legend
+    },
+    closePersonCard() {
+      if (this.selectedTable) {
+        this.setSelectedTable(null)
+      }
+    },
+    setSelectedTable(val) {
+      this.selectedTable = val
+    },
+    updateLegendSorting(val) {
+      this.legend = val
+    }
+  }
+}
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,55 +1,122 @@
 <template>
-    <div class="map">
-        <h3>Карта офиса</h3>
-
-        <div
-            v-if="!isLoading"
-            class="map-root"
-        >
-            <!-- map -->
-        </div>
-        <div v-else>Loading...</div>
+  <div class="map">
+    <h3>Карта офиса</h3>
+    <div v-if="!isLoading" class="map-root">
+      <MapSVG ref="svg" @click.stop="mapClickHandler" />
+      <Table v-show="false" ref="table" />
     </div>
+    <div v-else>Loading...</div>
+  </div>
 </template>
 
 <script>
+import * as d3 from 'd3'
+import MapSVG from '@/assets/images/map.svg'
+import Table from '@/assets/images/workPlace.svg'
+
 export default {
-    data() {
-        return {
-            isLoading: false,
-        };
+  name: 'Map',
+  components: {
+    MapSVG,
+    Table
+  },
+  props: {
+    tables: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isLoading: false,
+      svg: null,
+      g: null,
+      tableSVG: null
+    }
+  },
+  mounted() {
+    try {
+      this.isLoading = true
+      this.svg = d3.select(this.$refs.svg)
+      this.g = this.svg.select('g')
+      this.tableSVG = d3.select(this.$refs.table) // получаем шаблон стола
+      if (this.g) {
+        this.drawTables()
+      } else {
+        throw new Error('SVG is incorrect')
+      }
+    } catch (err) {
+      console.warn(err)
+    } finally {
+      this.isLoading = false
+    }
+  },
+  methods: {
+    updateUserSelection(val) {
+      this.$emit('tableSelected', val)
     },
-};
+    mapClickHandler($event) {
+      const tableEl = $event.target.classList.contains('.employer-place')
+        ? $event.target
+        : $event.target.closest('.employer-place')
+      if (!tableEl) {
+        this.updateUserSelection(null)
+      } else {
+        const tableObject = this.tables.find(t => t._id === +tableEl.id) ?? null
+        this.updateUserSelection(tableObject)
+      }
+    },
+    drawTables() {
+      const svgTablesGroupPlace = this.g
+
+      this.tables.map(table => {
+        svgTablesGroupPlace
+          .append('g')
+          .attr(
+            'transform',
+            `translate(${table.x}, ${table.y}) scale(0.5) rotate(${
+              table.rotate || 0
+            })`
+          )
+          .attr('fill', table.color)
+          .attr('id', table._id)
+          .classed('employer-place', true)
+          .classed('table', true)
+          .html(this.tableSVG.html())
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
 .map {
-    height: 100%;
-    width: 100%;
-    padding: 24px;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 24px;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
 }
 
 .map-root {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-    box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 ::v-deep svg {
-    height: 100%;
-    width: 100%;
+  height: 100%;
+  width: 100%;
 }
 
 ::v-deep .table {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
